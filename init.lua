3b5e6734daa9553e1b78dff5f90604d66560b151
Kevin <kjrodgers26@gmail.com>
2025-05-11 22:22:09 -0400

    added github color scheme

--- init.lua
+++ init.lua
@@ -342,6 +342,23 @@ require('lazy').setup({
       },
     },
   },
+  -- Install without configuration
+  { 'projekt0n/github-nvim-theme', name = 'github-theme' },
+
+  -- Or with configuration
+  {
+    'projekt0n/github-nvim-theme',
+    name = 'github-theme',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('github-theme').setup {
+        -- ...
+      }
+
+      vim.cmd 'colorscheme github_dark_default'
+    end,
+  },
 
   -- NOTE: Plugins can specify dependencies.
   --
